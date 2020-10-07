a8bc65465978f52b0e6db9178ef493bcb707a2ff
Alvaro de Francisco <alfrasan@gmail.com>
2020-10-07 11:05:55 +0200

    RecyclerView - First Attempt

---
+++ app/src/main/java/alfrasan/novatada/musicapitestjava/RecyclerViewAdapterSong.java
@@ -0,0 +1,65 @@
+package alfrasan.novatada.musicapitestjava;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapterSong extends RecyclerView.Adapter<RecyclerViewAdapterSong.ViewHolder> {
+
+    private Context context;
+    private ArrayList<Song> songList;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView songName;
+        private TextView songGroup;
+        private TextView songPath;
+
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+            songName = itemView.findViewById(R.id.item_txtView_songname);
+            songGroup = itemView.findViewById(R.id.item_txtView_songgroup);
+            songPath = itemView.findViewById(R.id.item_txtView_songpath);
+
+        }
+
+    }
+
+    public RecyclerViewAdapterSong(Context context, ArrayList<Song> songList) {
+
+        this.context = context;
+        this.songList = songList;
+
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapterSong.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_song_recyclerview, parent, false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapterSong.ViewHolder holder, int position) {
+
+        holder.songName.setText(songList.get(position).getName());
+        holder.songGroup.setText(songList.get(position).getGroup());
+        holder.songPath.setText(songList.get(position).getPath());
+
+    }
+
+    @Override
+    public int getItemCount() { return songList.size(); }
+
+}
