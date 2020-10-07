7684b2eb47a2e002e40e1c278e3e6eae7e44e005
Alvaro de Francisco <alfrasan@gmail.com>
2020-10-07 13:07:09 +0200

    RecyclerView - Second Attempt

--- app/src/main/java/alfrasan/novatada/musicapitestjava/ActivityMain.java
+++ app/src/main/java/alfrasan/novatada/musicapitestjava/ActivityMain.java
@@ -3,19 +3,16 @@ package alfrasan.novatada.musicapitestjava;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.RecyclerView.Adapter;
-import androidx.recyclerview.widget.RecyclerView.LayoutManager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
-import java.util.ArrayList;
-
 public class ActivityMain extends AppCompatActivity {
+
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +20,9 @@ public class ActivityMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ----------- TESTING CODE FOR THE RECYCLERVIEW ------------
-        Song song1 = new Song("aaaaaa", "bbbbbb", "C:/");
-        Song song2 = new Song("bbbbbbb", "DDDDDDDD", "Z:/");
-        ArrayList<Song> songList = new ArrayList<>();
-        songList.add(song1);
-        songList.add(song2);
-        // ----------- END OF TESTING CODE FOR THE RECYCLERVIEW ------------
-
-        final RecyclerView recyclerView = new RecyclerView(this);
-
         final TabLayout tabLayout = findViewById(R.id.tab_layout_main);
-        recyclerView.findViewById(R.id.recview_frag_song);
 
         final FragmentManager fragMag = getSupportFragmentManager();
-        final LayoutManager layoutManager = new LinearLayoutManager(this);
-        final Adapter<RecyclerViewAdapterSong.ViewHolder> recyclerViewAdapterSong = new RecyclerViewAdapterSong(this, songList);
-
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(recyclerViewAdapterSong);
 
         Toast.makeText(ActivityMain.this, R.string.thanks_app, Toast.LENGTH_SHORT).show();
 
@@ -61,7 +41,7 @@ public class ActivityMain extends AppCompatActivity {
                         break;
 
                     case 1:
-                        fragTransaction.replace(R.id.frag_layout_main, new FragmentSongs());
+                        fragTransaction.replace(R.id.frag_layout_main, new FragmentSongs(context));
                         break;
 
                     case 2: // TODO Add the third Fragment when created
--- app/src/main/java/alfrasan/novatada/musicapitestjava/FragmentSongs.java
+++ app/src/main/java/alfrasan/novatada/musicapitestjava/FragmentSongs.java
@@ -1,20 +1,47 @@
 package alfrasan.novatada.musicapitestjava;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class FragmentSongs extends Fragment {
 
-    public FragmentSongs() { }
+    private Context context;
+
+    public FragmentSongs(Context context) { this.context = context; }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        // ----------- TESTING CODE FOR THE RECYCLERVIEW ------------
+        Song song1 = new Song("aaaaaa", "bbbbbb", "C:/");
+        Song song2 = new Song("bbbbbbb", "DDDDDDDD", "Z:/");
+        ArrayList<Song> songList = new ArrayList<>();
+        songList.add(song1);
+        songList.add(song2);
+        // ----------- END OF TESTING CODE FOR THE RECYCLERVIEW ------------
+
+        final RecyclerView recyclerView = new RecyclerView(context);
+        recyclerView.findViewById(R.id.recview_frag_song);
+
+        final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+        final RecyclerView.Adapter<RecyclerViewAdapterSong.ViewHolder> recViewAdapter =
+                new RecyclerViewAdapterSong(context, songList);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recViewAdapter);
+
         return inflater.inflate(R.layout.fragment_song, container, false);
 
     }
+
 }
